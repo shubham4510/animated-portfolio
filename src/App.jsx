a1e5375a650e3946364237bdac8a123a66ff3a1f
Shubham <shubhambhardwaj112277@gmail.com>
2024-05-19 20:01:20 +0530

    Added Parallax Effect

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Test from "../Test";
 import "./app.scss"
 import Hero from "./components/hero/Hero";
 import Navbar from "./components/navbar/Navbar";
+import Parallax from "./components/parallax/Parallax";
 
 const App = () => {
   return <div>
@@ -9,7 +10,9 @@ const App = () => {
       <Navbar/>
       <Hero/>
     </section>
-    <section id="Services">Parallex</section>
+    <section id="Services">
+      <Parallax/>
+    </section>
     <section>Services</section>
     <section id="Portfolio">Parallax</section>
     <section>Portfolio1</section>
